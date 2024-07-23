64cff2a5a6b34c9ead9bb5ee576964cbbe3deefe
Heather <gitmail@emptystar.email>
2024-07-22 22:27:26 -0400

    Add soup recipes, add mod dependencies, adjust food healing amounts

--- init.lua
+++ init.lua
@@ -1,17 +1,46 @@
+-- Soup items
 minetest.register_craftitem("soup:tomato_soup", {
     description = "Tomato Soup",
     inventory_image = "tomato_soup.png",
-    on_use = minetest.item_eat(4),
+    on_use = minetest.item_eat(5,"ethereal:bowl"),
 })
 
 minetest.register_craftitem("soup:mushroom_soup", {
     description = "Mushroom Soup",
     inventory_image = "mushroom_soup.png",
-    on_use = minetest.item_eat(4),
+    on_use = minetest.item_eat(5,"ethereal:bowl"),
 })
 
 minetest.register_craftitem("soup:chicken_noodle_soup", {
     description = "Chicken Noodle Soup",
     inventory_image = "chicken_noodle_soup.png",
-    on_use = minetest.item_eat(20),
+    on_use = minetest.item_eat(8,"ethereal:bowl"),
+})
+
+-- Soup recipes
+minetest.register_craft({
+	output = "soup:tomato_soup",
+	recipe = {
+		{"farming:tomato","farming:tomato", "farming:tomato"},
+		{"","bottles:bottle_of_water", ""},
+		{"","group:food_bowl", ""}
+	}
+})
+
+minetest.register_craft({
+	output = "soup:mushroom_soup",
+	recipe = {
+		{"group:food_mushroom","group:food_mushroom", "group:food_mushroom"},
+		{"","bottles:bottle_of_water", ""},
+		{"","group:food_bowl", ""}
+	}
+})
+
+minetest.register_craft({
+	output = "soup:chicken_noodle_soup",
+	recipe = {
+		{"","animalia:poultry_cooked", ""},
+		{"","bottles:bottle_of_water", ""},
+		{"","group:food_bowl", ""}
+	}
 })
